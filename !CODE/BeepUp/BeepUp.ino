ef77f0b934cdbf3ae5e19e459297ec7120251475
mikestebbins <michaelstebbins@gmail.com>
2016-03-28 23:10:24 -0700

    option to turn LED on or not, comment cleaning

--- !CODE/BeepUp/BeepUp.ino
+++ !CODE/BeepUp/BeepUp.ino
@@ -1,38 +1,47 @@
 /*
+ * 
 Plays a construction vehicle back-up tone when the button is held depressed
+in order to delight a 2-old-boy
 
  circuit:
- * 8-ohm speaker on digital pin e
+ * 8-ohm speaker attached to Gnd and Pin 0
+ * button attached to Gnd and Pin 2
 
- created 2016-03-27
+ created 2016-03-28
  by Mike Stebbins
-TODO FIX THIS Very roughly based on code from Tom Igoe (http://www.arduino.cc/en/Tutorial/Tone)
- */
-
+based roughly on Trinket sound code by: // http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
+*/
 
 #define tonePin 0
 #define ledPin 1
 #define buttonPin 2 
 #define frequency 3000
-#define onTime 300
-#define offTime 300
+#define onTime 250
+#define offTime 250
+#define ledOnOrOff 1
 
 void setup() {
   pinMode(buttonPin,INPUT_PULLUP);
   pinMode(tonePin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  if (ledOnOrOff == 1)  {
+    pinMode(ledPin, OUTPUT);
+  }
   delay(100);
 }
 
 void loop() {
   boolean buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH)  {
-    digitalWrite(ledPin,HIGH);
+    if (ledOnOrOff == 1)  {
+      digitalWrite(ledPin,HIGH);
+    }
     beep(tonePin,frequency,onTime);
     delay(offTime);
     }
     else  {
-      digitalWrite(ledPin,LOW);
+      if (ledOnOrOff == 1)  {
+        digitalWrite(ledPin,LOW);
+      }
       delay (250);  
     }
     delay(10);
@@ -40,7 +49,7 @@ void loop() {
 
 // the sound producing function
 void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds)
-{   // http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
+{   
           int x;   
           long delayAmount = (long)(1000000/frequencyInHertz);
           long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2));
